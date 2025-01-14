a97a8516df1ae8a9ae5318645880b8ea0a62407c
chhunhychhunhy121828@gmail.com <chhunhychhunhy121828@gmail.com>
2025-01-14 14:17:18 +0700

    more

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,18 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  resolve: {
-    alias: {
-      // Point directly to the react-router-dom package
-      'react-router-dom': 'react-router-dom', 'react-redux': 'react-redux',
-    },
-  },
-  build: {
-    rollupOptions: {
-      external: ['react-redux','@reduxjs/toolkit','formik','yub','react-toastify','react-slick'],
-    },
-  },
+  // base: '/',
+  // resolve: {
+  //   alias: {
+  //     // Point directly to the react-router-dom package
+  //     'react-router-dom': 'react-router-dom', 'react-redux': 'react-redux',
+  //   },
+  // },
+  // build: {
+  //   outDir: 'dist', // The default output folder for Vite builds
+  //   rollupOptions: {
+      
+  //     external: ['react-redux','@reduxjs/toolkit','formik','yub','react-toastify','react-slick'],
+  //   },
+  // },
 });
